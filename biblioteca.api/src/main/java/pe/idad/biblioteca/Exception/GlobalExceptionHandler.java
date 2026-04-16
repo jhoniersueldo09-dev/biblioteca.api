99a8cb6c7814e058602fccf54089d0003084dd86
jhonnier <jhoniersueldo09@gmail.com>
2026-04-16 18:03:25 -0500

    Actualización de GlobalExeptionhandler

--- biblioteca.api/src/main/java/pe/idad/biblioteca/Exception/GlobalExceptionHandler.java
+++ biblioteca.api/src/main/java/pe/idad/biblioteca/Exception/GlobalExceptionHandler.java
@@ -11,6 +11,7 @@ import java.util.Map;
 @RestControllerAdvice
 public class GlobalExceptionHandler {
 
+    // Maneja errores de validación de los DTOs
     @ExceptionHandler(MethodArgumentNotValidException.class)
     public Map<String, Object> manejarValidaciones(MethodArgumentNotValidException ex) {
 
@@ -22,6 +23,16 @@ public class GlobalExceptionHandler {
 
         Map<String, Object> response = new HashMap<>();
         response.put("errores", errores);
+
+        return response;
+    }
+    // maneja excepciones en tiempo de ejecución generadas
+    // en el service o cualquier parte de la aplicación.
+    @ExceptionHandler(RuntimeException.class)
+    public Map<String, Object> manejarRuntime(RuntimeException ex) {
+
+        Map<String, Object> response = new HashMap<>();
+        response.put("error", ex.getMessage());
 
         return response;
     }
